33a395ca7f67b9883c9edb52dbd404ffbaa10967
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-12-12 01:19:06 +0100

    added s3 and dynamodb outputs

--- output.tf
+++ output.tf
@@ -33,3 +33,11 @@ output "role_name" {
 output "role_arn" {
   value = var.create_base_role == true ? aws_iam_role.base_role[0].arn : null
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.state_storage.id
+}
+
+output "lock_table_name" {
+  value = aws_dynamodb_table.terraform_lock.id
+}
