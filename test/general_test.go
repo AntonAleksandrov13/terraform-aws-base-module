20114026999aa0e5a8034644850ae03151579f58
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-12-16 23:28:23 +0100

    WIP: working on test for assume role

--- test/general_test.go
+++ test/general_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -11,21 +12,11 @@ import (
 )
 
 func TestOnlyRoleCreation(t *testing.T) {
-	svc := sts.New(session.Must(session.NewSession()))
-	input := &sts.GetCallerIdentityInput{}
-
-	result, err := svc.GetCallerIdentity(input)
+	sess := session.Must(session.NewSession())
+	callerAccount, err := GetAWSAccountNumber(sess)
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				t.Errorf(aerr.Error())
-			}
-		} else {
-			t.Errorf(err.Error())
-		}
+		t.Error(err)
 	}
-	callerAccount := *result.Account
 	//Construct the terraform options with default retryable errors to handle the most common
 	//retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
@@ -48,25 +39,12 @@ func TestOnlyRoleCreation(t *testing.T) {
 }
 
 func TestExistingUserCanAssumeRole(t *testing.T) {
-	svc := sts.New(session.Must(session.NewSession()))
-	input := &sts.GetCallerIdentityInput{}
-
-	result, err := svc.GetCallerIdentity(input)
+	sess := session.Must(session.NewSession())
+	callerAccount, err := GetAWSAccountNumber(sess)
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				t.Errorf(aerr.Error())
-			}
-		} else {
-			t.Errorf(err.Error())
-		}
+		t.Error(err)
 	}
-	callerAccount := *result.Account
-	//Construct the terraform options with default retryable errors to handle the most common
-	//retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "../examples/existing_user_can_assume_role",
 	})
 
@@ -76,10 +54,23 @@ func TestExistingUserCanAssumeRole(t *testing.T) {
 	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform roleNameReturned` to get the values of roleNameReturned variables and check they have the expected values.
 	roleNameReturned := terraform.Output(t, terraformOptions, "role_name")
 	assert.Equal(t, "terraform", roleNameReturned)
 
 	roleARNReturned := terraform.Output(t, terraformOptions, "role_arn")
 	assert.Equal(t, fmt.Sprintf("arn:aws:iam::%v:role/terraform", callerAccount), roleARNReturned)
+
+}
+
+func GetAWSAccountNumber(session client.ConfigProvider) (string, error) {
+	svc := sts.New(session)
+	input := &sts.GetCallerIdentityInput{}
+
+	result, err := svc.GetCallerIdentity(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			return "", aerr
+		}
+	}
+	return *result.Account, nil
 }
