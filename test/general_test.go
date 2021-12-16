245e5626859c66666b76e9247dfb13c38c99c0ca
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-12-16 22:57:56 +0100

    reworking test structure

--- test/general_test.go
+++ test/general_test.go
@@ -10,8 +10,6 @@ import (
 	"testing"
 )
 
-var testPath = "../examples/only_role"
-
 func TestOnlyRoleCreation(t *testing.T) {
 	svc := sts.New(session.Must(session.NewSession()))
 	input := &sts.GetCallerIdentityInput{}
@@ -32,7 +30,44 @@ func TestOnlyRoleCreation(t *testing.T) {
 	//retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
-		TerraformDir: testPath,
+		TerraformDir: "../examples/only_role",
+	})
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform roleNameReturned` to get the values of roleNameReturned variables and check they have the expected values.
+	roleNameReturned := terraform.Output(t, terraformOptions, "role_name")
+	assert.Equal(t, "terraform", roleNameReturned)
+
+	roleARNReturned := terraform.Output(t, terraformOptions, "role_arn")
+	assert.Equal(t, fmt.Sprintf("arn:aws:iam::%v:role/terraform", callerAccount), roleARNReturned)
+}
+
+func TestExistingUserCanAssumeRole(t *testing.T) {
+	svc := sts.New(session.Must(session.NewSession()))
+	input := &sts.GetCallerIdentityInput{}
+
+	result, err := svc.GetCallerIdentity(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				t.Errorf(aerr.Error())
+			}
+		} else {
+			t.Errorf(err.Error())
+		}
+	}
+	callerAccount := *result.Account
+	//Construct the terraform options with default retryable errors to handle the most common
+	//retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../examples/existing_user_can_assume_role",
 	})
 
 	// Clean up resources with "terraform destroy" at the end of the test.
