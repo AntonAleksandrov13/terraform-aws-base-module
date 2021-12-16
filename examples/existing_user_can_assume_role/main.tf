85b566e76bd26ad0dab8734aa4a6fe963825b5e4
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-12-16 23:16:31 +0100

    added outputs for tests

--- examples/existing_user_can_assume_role/main.tf
+++ examples/existing_user_can_assume_role/main.tf
@@ -17,9 +17,17 @@ resource "aws_iam_user" "test_user" {
   path = "/"
 }
 
-module "base-module" {
+module "base_module" {
   source            = "../.."
   create_base_role  = true
   allow_user_assume = true
   base_user_name = aws_iam_user.test_user.name
+}
+
+output "role_name" {
+  value = module.base_module.role_name
+}
+
+output "role_arn" {
+  value = module.base_module.role_arn
 }
